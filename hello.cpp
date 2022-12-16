ee071244f9cb4c339b4100bd8ef479aa96c06be5
Arbaz <arbaz582021@gmail.com>
2022-12-16 14:22:57 +0500

    add

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+string name;
+cout<<"what is your name ";
+cin>>name;
+cout<<"your name is"<<name;
+return 0;
+}
+
---
+++ task1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"________________"<<endl;
+
+cout<<"|               |"<<endl;
+cout<<"|               |";
+cout<<"________________";
+return 0;
+
+
+
+}
